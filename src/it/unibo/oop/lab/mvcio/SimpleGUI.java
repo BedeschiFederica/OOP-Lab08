c3d1daefe61247ec51544963f3e8bec479a7eca0
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2021-12-13 11:33:17 +0100

    Complete class SimpleGUI

--- src/it/unibo/oop/lab/mvcio/SimpleGUI.java
+++ src/it/unibo/oop/lab/mvcio/SimpleGUI.java
@@ -1,9 +1,16 @@
 package it.unibo.oop.lab.mvcio;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -11,7 +18,7 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private final JFrame frame = new JFrame("My first Java graphical interface");
 
     /*
      * Once the Controller is done, implement this class in such a way that:
@@ -35,8 +42,10 @@ public final class SimpleGUI {
 
     /**
      * builds a new {@link SimpleGUI}.
+     * @param controller
+     *          the controller that controls the I/O access.
      */
-    public SimpleGUI() {
+    public SimpleGUI(final Controller controller) {
         /*
          * Make the frame half the resolution of the screen. This very method is
          * enough for a single screen setup. In case of multiple monitors, the
@@ -50,13 +59,49 @@ public final class SimpleGUI {
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         final int sw = (int) screen.getWidth();
         final int sh = (int) screen.getHeight();
-        frame.setSize(sw / 2, sh / 2);
+        this.frame.setSize(sw / 2, sh / 2);
         /*
          * Instead of appearing at (0,0), upper left corner of the screen, this
          * flag makes the OS window manager take care of the default positioning
          * on screen. Results may vary, but it is generally the best choice.
          */
-        frame.setLocationByPlatform(true);
+        this.frame.setLocationByPlatform(true);
+
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        final JTextArea text = new JTextArea();
+        final JButton save = new JButton("Save");
+        panel.add(text, BorderLayout.CENTER);
+        panel.add(save, BorderLayout.SOUTH);
+        this.frame.setContentPane(panel);
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    controller.write(text.getText());
+                } catch (IOException exc) {
+                    System.out.println(exc.getMessage());
+                }
+            }
+        });
+    }
+
+    /**
+     * displays the frame.
+     */
+    public void display() {
+        this.frame.setVisible(true);
+    }
+
+    /**
+     * @param args
+     *          ignored
+     */
+    public static void main(final String... args) {
+        final Controller controller = new Controller();
+        final SimpleGUI gui = new SimpleGUI(controller);
+        gui.display();
     }
 
 }
