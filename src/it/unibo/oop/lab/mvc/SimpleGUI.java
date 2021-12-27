5cb46b1255deb71425f8c96f30f856542820a78e
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2021-12-27 11:32:26 +0100

    Complete class SimpleGUI

--- src/it/unibo/oop/lab/mvc/SimpleGUI.java
+++ src/it/unibo/oop/lab/mvc/SimpleGUI.java
@@ -1,9 +1,17 @@
 package it.unibo.oop.lab.mvc;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  * A very simple program using a graphical interface.
@@ -16,7 +24,7 @@ public final class SimpleGUI {
     /*
      * Once the Controller is done, implement this class in such a way that:
      * 
-     * 1) I has a main method that starts the graphical application
+     * 1) It has a main method that starts the graphical application
      * 
      * 2) In its constructor, sets up the whole view
      * 
@@ -36,8 +44,10 @@ public final class SimpleGUI {
 
     /**
      * builds a new {@link SimpleGUI}.
+     * @param controller
+     *          the controller of the application
      */
-    public SimpleGUI() {
+    public SimpleGUI(final ControllerImpl controller) {
 
         /*
          * Make the frame half the resolution of the screen. This very method is
@@ -60,6 +70,58 @@ public final class SimpleGUI {
          * on screen. Results may vary, but it is generally the best choice.
          */
         frame.setLocationByPlatform(true);
+
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        final JTextField stringText = new JTextField();
+        final JTextArea historyText = new JTextArea();
+        historyText.setEditable(false);
+        final JButton printButton = new JButton("Print");
+        final JButton historyButton = new JButton("Show history");
+        mainPanel.add(stringText, BorderLayout.NORTH);
+        mainPanel.add(historyText, BorderLayout.CENTER);
+        final JPanel southPanel = new JPanel();
+        southPanel.add(printButton);
+        southPanel.add(historyButton);
+        mainPanel.add(southPanel, BorderLayout.SOUTH);
+        this.frame.setContentPane(mainPanel);
+        printButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                controller.setStringToPrint(stringText.getText());
+                controller.printString();
+            }
+        });
+        historyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                final StringBuilder history = new StringBuilder();
+                final List<String> strings = controller.getPrintedStringsHistory();
+                for (final String str: strings) {
+                    history.append(str);
+                    history.append('\n');
+                }
+                historyText.setText(history.toString());
+            }
+        });
+    }
+
+    /**
+     * Displays the GUI.
+     */
+    private void display() {
+        this.frame.setVisible(true);
+    }
+
+    /**
+     * Starts the graphical application.
+     * @param args
+     *          ignored
+     */
+    public static void main(final String[] args) {
+        final SimpleGUI gui = new SimpleGUI(new ControllerImpl());
+        gui.display();
     }
 
 }
